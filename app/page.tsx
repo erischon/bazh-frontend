8fbb774b824dfea3e2b357901f7c2587641f4e3e
Eri Schon <erischon@gmail.com>
2023-02-11 16:53:38 +0100

    migration to React and setup

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1 className="text-4xl">Welcome to the Blog</h1>
-    </main>
-  );
-}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="App">
+      <h1>Home Bazh</h1>
+    </main>
+  );
+}
+
+export default App;
